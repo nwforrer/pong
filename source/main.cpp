d801138b6288d1c5323ba1c26524e62b3e90cbcc
Nick Forrer <nwforrer@gmail.com>
2015-07-02 14:51:44 -0400

    Convert to optimized surface for blitting.

--- source/main.cpp
+++ source/main.cpp
@@ -68,13 +68,25 @@ void close()
 
 SDL_Surface* loadSurface(std::string path)
 {
+  SDL_Surface* optimizedSurface = NULL;
+
   SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
   if (loadedSurface == NULL)
   {
     printf("Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
   }
+  else
+  {
+    optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
+    if (optimizedSurface == NULL)
+    {
+      printf("Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+    }
 
-  return loadedSurface;
+    SDL_FreeSurface(loadedSurface);
+  }
+
+  return optimizedSurface;
 }
 
 int main(int argc, char **argv)
